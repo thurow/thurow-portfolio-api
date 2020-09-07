556ee91af8819738715a323ab49c6caa71515c82
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2020-09-07 18:15:22 -0300

    Add tsconfig-paths

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use('/', (req, res) => {
-    return res.json({ message: 'Welcome to Thurow Portfolio\'s api =)' })
+  return res.json({ message: 'Welcome to Thurow Portfolio\'s api =)' })
 })
 
 app.listen(3333)
