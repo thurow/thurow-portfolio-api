128e209afd59c029184c1e6eec047dedee9033ba
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2020-09-07 19:00:35 -0300

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { compilerOptions } = require('./tsconfig.json')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+module.exports = {
+  clearMocks: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  preset: 'ts-jest',
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+    '!**/node_modules/**'
+  ]
+};
